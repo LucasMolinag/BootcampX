d19f4160f3aec6007d0669057a2a4ab863be1e62
Lucas <molinag.lucas@gmail.com>
2023-06-16 13:35:04 -0700

    Parameterize code

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,15 +6,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`];
+
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON students_id = students.id 
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '%${process.argv[2]}% || 'JUL02'
+WHERE cohorts.name = $1 || 'JUL02'
 ORDER BY teachers.name;
-`)
+`, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
